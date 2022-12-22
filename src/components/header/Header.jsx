b837d9cd55e7ed60c27452b01c6b56467a28b723
turgunboev70 <turgunboevjr@gmail.com>
2022-12-22 22:12:17 +0500

    feauture-headerPages

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderTop from './header-top/HeaderTop'
+
+const Header = () => {
+  return (
+    <>
+        <HeaderTop/>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/header/header-top/HeaderTop.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import c from "./HeaderTop.module.css"
+import { FiPhone } from "react-icons/fi"
+import { SlLocationPin } from "react-icons/sl"
+import logo from "../../../assets/images/texnomart_logo.svg"
+import headerPages from "../../../dummy-files/headerTop.dummy.json"
+
+const HeaderTop = () => {
+  const locationNames = ["Toshkent", "Buxoro", "Andijon", "Fergana", "Namangan", "Samarqand"]
+  const [userLocation, setUserLocation] = useState("Toshkent")
+
+
+  return (
+    <div className={c.header__top}>
+      <div className='container'>
+        <div className={c.header__topInner}>
+          <span className={c.header__logo__left}>
+            <a className={c.header__logo__link} href="/">
+              <img src={logo} alt="loading..." width="152" height="32" />
+            </a>
+          </span>
+          <div className={c.header__top__right}>
+            <span className={c.header__right__icon}>
+              <a href="tel:+998941245559">
+                <FiPhone />
+              </a>
+            </span>
+            <div className={c.header__top__location}>
+              <button className={c.header__region}>
+                <SlLocationPin className={c.header__userLocation}/>
+                <span>
+                  {userLocation}
+                </span>
+              </button>
+              <div className={c.header__location__dropdown}>
+                {locationNames.map(city =>
+                  <div onClick={() => setUserLocation(city)} className={c.header__dropdown__city}>
+                    {city}
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className={c.header__top__pages}>
+                  <ul className={c.header__top__list}>
+                    {headerPages.map(({text, background}) => 
+                    <li className={`${c.header__pages}`}>{text}</li>
+                    )}
+                  </ul>
+            </div>
+          </div>
+          <div className={c.header__top__call}>
+            <a href="tel:+998941245559">
+              <FiPhone className={c.call__icon}/>
+              <span>+99894 124 55 59</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderTop
